466c351c476471acaded860e3b7051053566c303
Dragos Nedelcu <dragosgn@gmail.com>
2018-03-13 10:22:44 +0100

    thrid commit

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require('path')
 const { app, BrowserWindow, Menu } = electron
 
 let mainWindow
+let addWidnow
 
 // Listen for app to be ready
 app.on('ready', function() {
@@ -23,19 +24,36 @@ app.on('ready', function() {
 	Menu.setApplicationMenu(mainMenu)
 })
 
+// Handle add window
+function createAddWindow() {
+	mainWindow = new BrowserWindow({})
+	// Load html file into window
+	mainWindow.loadURL(
+		url.format({
+			pathname: path.join(__dirname, 'mainWindow.html'),
+			protocol: 'file:',
+			slashes: true
+		})
+	)
+}
+
 // Create menu template
 const mainMenuTemplate = [
 	{
 		label: 'File',
 		submenu: [
 			{
-				label: 'Add Item'
+				label: 'Add Item',
+				click() {
+					createAddWindow()
+				}
 			},
 			{
 				label: 'Clear Items'
 			},
 			{
 				label: 'Quit',
+				accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+!',
 				click() {
 					app.quit()
 				}
