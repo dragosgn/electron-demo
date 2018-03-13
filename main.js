8598af719a8af56eee1a0908b5bc82c9a4bc0114
Dragos Nedelcu <dragosgn@gmail.com>
2018-03-13 11:23:14 +0100

    adding itme respinse

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron')
 const url = require('url')
 const path = require('path')
 
-const { app, BrowserWindow, Menu } = electron
+const { app, BrowserWindow, Menu, ipcMain } = electron
 
 let mainWindow
 let addWindow
@@ -48,6 +48,12 @@ function createAddWindow() {
 		addWindow = null
 	})
 }
+
+// Catch item: add
+ipcMain.on('item:add', function(e, item) {
+	mainWindow.webContents.send('item:add', item)
+	addWindow.close()
+})
 
 // Create menu template
 const mainMenuTemplate = [
