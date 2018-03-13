58d959a818287f7011efbf320c4efd8a1d300f3c
Dragos Nedelcu <dragosgn@gmail.com>
2018-03-13 10:15:26 +0100

    seconfd cmmiit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron')
 const url = require('url')
 const path = require('path')
 
-const { app, BrowserWindow } = electron
+const { app, BrowserWindow, Menu } = electron
 
 let mainWindow
 
@@ -18,4 +18,14 @@ app.on('ready', function() {
 			slashes: true
 		})
 	)
+	// Build menu from template
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+	Menu.setApplicationMenu(mainMenu)
 })
+
+// Create menu template
+const mainMenuTemplate = [
+	{
+		label: 'File'
+	}
+]
